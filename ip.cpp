ff220de60decda45471c950bc77b367388cd9774
Baalateja Kataru <baalateja.k@gmail.com>
2024-11-12 10:12:30 +0000

    ip.cpp - convert between decimal/binary IP representations

--- ip.cpp
+++ ip.cpp
@@ -1,46 +1,81 @@
 #include <string>
 #include <iostream>
+#include <cmath>
 
-std::string decimalToBinary(int decimal)
+std::string decimalToBinary(unsigned int decimal)
 {
+    if (decimal == 0) return "0";
+
     std::string binaryString;
 
     while (decimal > 0)
     {
-        binaryString.push_back((char) decimal % 2);
+        binaryString = std::to_string(decimal % 2) + binaryString;
         decimal /= 2;
     }
 
     return binaryString;
 }
 
-int IPToBinary(std::string ipAddress)
+std::string padOctet(std::string octetString)
 {
+    while (octetString.length() < 8) octetString = '0' + octetString;
+
+    return octetString;
+}
+
+unsigned int binaryToDecimal(std::string binary)
+{
+    unsigned int decimalVal = 0;
+
+    for(size_t i = 0; i < binary.length(); i++)
+    {
+        decimalVal += (unsigned int) (pow(2, binary.length() - i - 1) * (binary[i] - '0'));    
+    }
+
+    return decimalVal;
+}
+
+std::string IPToBinary(std::string ipAddress)
+{
+    std::string binaryString;
     size_t ptr = 0;
+
+    ipAddress += '.'; // TODO: this is a hack, write a more general solution taking into account the end of the string
     for(size_t i = 0; i < ipAddress.length(); i++)
     {
-        if (ipAddress[i] == '.' || i == ipAddress.length() - 1)
+        if (ipAddress[i] == '.')
         {
-            std::cout << ipAddress.substr(ptr, i - ptr) << std::endl;
+            unsigned int octet = std::stoi(ipAddress.substr(ptr, i - ptr));
 
-            int octet = std::stoi(ipAddress.substr(ptr, i));
-
-            // std::cout << octet << std::endl;
+            binaryString += padOctet(decimalToBinary(octet));
 
             ptr = i + 1;
         }
     }
 
-    return 0;
+    return binaryString;
+}
+
+std::string binaryToIP(std::string binaryString)
+{
+    std::string ipAddress;
+
+    for(int i = 0; i < 4; i++)
+    {
+        ipAddress += std::to_string(binaryToDecimal(binaryString.substr(i * 8, 8))) + '.';
+    }
+
+    ipAddress.pop_back(); // TODO: this is a hack, write a more general solution taking into account the end of the string
+
+    return ipAddress;
 }
 
 int main()
 {
     std::string testAddress = "192.168.1.1";
 
-    std::cout << decima
+    std::cout << IPToBinary(testAddress) << ", length: " << IPToBinary(testAddress).length() << std::endl;
 
-    IPToBinary(testAddress);
-
-    // std::cout << IPToBinary(testAddress) << std::endl;
+    std::cout << binaryToIP(IPToBinary(testAddress)) << std::endl;
 }  
